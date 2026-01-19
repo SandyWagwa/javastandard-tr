d739f7a98c232bd59391724a87efa760f9caca01
Sandra Wagwa <116626120+kamashu6793@users.noreply.github.com>
2026-01-19 09:08:42 +0300

    if statements

---
+++ controlflow/ifstatements.java
@@ -0,0 +1,43 @@
+package controlflow;
+
+public class ifstatements {
+    public static void main(String[] args) {
+        // int number = 10;
+
+        // // Simple if statement
+        // if (number > 0) {
+        // System.out.println("The number is positive.");
+        // }
+
+        // // if-else statement
+        // if (number % 2 == 0) {
+        // System.out.println("The number is even.");
+        // } else {
+        // System.out.println("The number is odd.");
+        // }
+
+        // // if-else-if ladder
+        // if (number > 0) {
+        // System.out.println("The number is positive.");
+        // } else if (number < 0) {
+        // System.out.println("The number is negative.");
+        // } else {
+        // System.out.println("The number is zero.");
+        // }
+
+        // int age = 25;
+        // if (age >= 18) {
+        // System.out.println("You can vote.");
+        // } else {
+        // System.out.println("You cannot vote.");
+        // }
+
+        int age = 20;
+        boolean hasID = true;
+        if (age >= 18) {
+            System.out.println("You can enter the club.");
+        } else {
+            System.out.println("You cannot enter the club.");
+        }
+    }
+}
