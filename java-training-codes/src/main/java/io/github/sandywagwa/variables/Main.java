e045287e15fc2dfb49d76cfee6133e65d7809f58
Sandra Wagwa <116626120+kamashu6793@users.noreply.github.com>
2026-01-20 18:27:26 +0300

    variables folder

---
+++ java-training-codes/src/main/java/io/github/sandywagwa/variables/Main.java
@@ -0,0 +1,19 @@
+package io.github.sandywagwa.variables;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age;
+        String name;
+        double salary;
+
+        age = 25;
+        name = "Sandy Wagwa";
+        salary = 55000.50;
+
+        // Declaration+ Assignment
+        int score = 100;
+        String message = "Hello";
+
+    }
+}
